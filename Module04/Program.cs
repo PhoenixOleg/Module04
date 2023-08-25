d76ce4660f86faa0ce36721e7744ca2577cf6adf
PhoenixOleg <metall_2000@mail.ru>
2023-08-26 00:31:01 +0300

    Задание 4.1.18 (switch)

--- Module04/Program.cs
+++ Module04/Program.cs
@@ -39,34 +39,66 @@ namespace Module04
             //Console.WriteLine(c);
 
             //Задание 4.1.17
+            //Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            //var color = Console.ReadLine();
+
+            //if (color == "red")
+            //{
+            //    Console.BackgroundColor = ConsoleColor.Red;
+            //    Console.ForegroundColor = ConsoleColor.Black;
+
+            //    Console.WriteLine("Your color is red!");
+            //}
+
+            //else if (color == "green")
+            //{
+            //    Console.BackgroundColor = ConsoleColor.Green;
+            //    Console.ForegroundColor = ConsoleColor.Black;
+
+            //    Console.WriteLine("Your color is green!");
+            //}
+            //else
+            //{
+            //    Console.BackgroundColor = ConsoleColor.Cyan;
+            //    Console.ForegroundColor = ConsoleColor.Black;
+
+            //    Console.WriteLine("Your color is cyan!");
+            //}
+
+
+            //Задание 4.1.18
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
             var color = Console.ReadLine();
 
-            if (color == "red")
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+			switch (color)
+			{
+                case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is red!");
+					Console.WriteLine("Your color is red!");
+				break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                break;
             }
-
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-            }
-
-
 
             Console.ReadKey();
         }
