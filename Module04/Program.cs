b0c9e644333a57bdef980918c0fdaf046321ba79
PhoenixOleg <metall_2000@mail.ru>
2023-08-25 23:55:14 +0300

    Задание 4.1.17

--- Module04/Program.cs
+++ Module04/Program.cs
@@ -26,17 +26,47 @@ namespace Module04
             //Console.WriteLine(c);
 
             //Задание 4.1.10
-            var inv = true;
-            var result = !inv;
-            Console.WriteLine(result);
+            //var inv = true;
+            //var result = !inv;
+            //Console.WriteLine(result);
 
             // Задание 4.1.15
-            var a = 6;
-            var b = 7;
+            //var a = 6;
+            //var b = 7;
 
-            var c = a != b ? a + b : b;
+            //var c = a != b ? a + b : b;
 
-            Console.WriteLine(c);
+            //Console.WriteLine(c);
+
+            //Задание 4.1.17
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+
+
 
             Console.ReadKey();
         }
